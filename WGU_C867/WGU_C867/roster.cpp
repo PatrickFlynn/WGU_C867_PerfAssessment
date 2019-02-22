1f5991f2800e8650354d7c299c8e12dfb69a1001
Patrick Flynn <patrickseanflynn@gmail.com>
2019-02-21 18:14:44 -0800

    testing memory addressing

--- WGU_C867/WGU_C867/roster.cpp
+++ WGU_C867/WGU_C867/roster.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include "student.h"
 #include <vector>
+#include <array>
 using namespace std;
 
-void main() {
+int main() {
+
 	cout << "Course Name: Scripting and Programming - Applications - C867" << endl;
 	cout << "Programming Language Used: C++ via Microsoft Visual Studio" << endl;
 	cout << "Student ID: #001086219" << endl;
 	cout << "Student Name: Patrick S. Flynn" << endl;
-	/*
+	
 	const string studentData[] =
 	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
 	"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 	"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
 	"A5,Patrick,Flynn,pflynn11@wgu.edu,27,25,35,50,SOFTWARE" };
+
+	/*
 
 	string* classRosterArray[] = &studentData;
 	*/
@@ -28,5 +32,22 @@ void main() {
 	cout << Patrick.getAge() << endl;
 	cout << Patrick.getEmail() << endl;
 	
-	return;
+
+	int i;
+	//int j;
+
+	cout << size(studentData);
+	
+	for (i = 0;  i < size(studentData); i++) {
+		cout << &studentData[i];
+		cout << "-" << studentData[i] << endl;
+	}
+	
+	int test = 15;
+	int* myptr;
+	myptr = &test;
+
+	cout << "Pointer address is: " << myptr << endl;
+
+	return 0;
 }
